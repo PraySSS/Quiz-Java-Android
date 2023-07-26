d970e136e06b1cac8b0be001fe83f9a31c6f2a82
Pimpaka Joungthong <pimpakaj@rabbit.co.th>
2023-07-26 17:11:33 +0700

    AddSomeQueries

---
+++ app/src/main/java/com/example/sqlitequiz/DBHelper.java
@@ -0,0 +1,67 @@
+package com.example.sqlitequiz;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.example.sqlitequiz.QuizContract.QuestionsTable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "Quiz.db";
+    private static final int DATABASE_VERSION = 1;
+    private SQLiteDatabase db;
+
+    public DBHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        this.db = db;
+
+        final String SQL_CREATE_QUESTIONS_TABLE = "CREATE TABLE " +
+                QuestionsTable.TABLE_NAME + " ( " +
+                QuestionsTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                QuestionsTable.COLUMN_QUESTION + " TEXT, " +
+                QuestionsTable.COLUMN_OPTION1 + " TEXT, " +
+                QuestionsTable.COLUMN_OPTION2 + " TEXT, " +
+                QuestionsTable.COLUMN_OPTION3 + " TEXT, " +
+                QuestionsTable.COLUMN_ANSWER_NR + " INTEGER" +
+                ")";
+
+        db.execSQL(SQL_CREATE_QUESTIONS_TABLE);
+        fillQuestionTable();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+db.execSQL("DROP TABLE IF EXISTS "+QuestionsTable.TABLE_NAME);
+    }
+
+    private void fillQuestionTable() {
+        QuestionModel q1 = new QuestionModel("A is correct", "A", "B", "C", 1);
+        addQuestion(q1);
+        QuestionModel q2 = new QuestionModel("B is correct", "A", "B", "C", 2);
+        addQuestion(q2);
+        QuestionModel q3 = new QuestionModel("C is correct", "A", "B", "C", 3);
+        addQuestion(q3);
+        QuestionModel q4 = new QuestionModel("A is correct again", "A", "B", "C", 1);
+        addQuestion(q4);
+        QuestionModel q5 = new QuestionModel("B is correct again", "A", "B", "C", 2);
+        addQuestion(q5);
+    }
+
+    private void addQuestion(QuestionModel question) {
+        ContentValues cv = new ContentValues();
+        cv.put(QuestionsTable.COLUMN_QUESTION, question.getQuestion());
+        cv.put(QuestionsTable.COLUMN_OPTION1, question.getOption1());
+        cv.put(QuestionsTable.COLUMN_OPTION2, question.getOption2());
+        cv.put(QuestionsTable.COLUMN_OPTION3, question.getOption3());
+        cv.put(QuestionsTable.COLUMN_ANSWER_NR, question.getAnswerNr());
+        db.insert(QuestionsTable.TABLE_NAME, null, cv);
+    }
+
+}
--- app/src/main/java/com/example/sqlitequiz/MainActivity.java
+++ app/src/main/java/com/example/sqlitequiz/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.sqlitequiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonStart=findViewById(R.id.btn_start_quiz);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startQuiz();
+            }
+        });
     }
+
+    private void startQuiz() {
+        Intent intent=new Intent(MainActivity.this,QuizActivity.class);
+        startActivity(intent);
+    }
+
+
 }
